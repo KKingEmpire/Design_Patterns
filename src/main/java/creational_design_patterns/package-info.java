f15aa8279318ed84d6154abd7b76c28e579f179a
Karnakannan <Karnakannan@users.noreply.github.com>
2019-12-03 07:37:17 +0800

    package info commit

--- src/main/java/creational_design_patterns/package-info.java
+++ src/main/java/creational_design_patterns/package-info.java
@@ -6,3 +6,8 @@
  *
  */
 package creational_design_patterns;
+
+/**
+Creational Design Patterns are concerned with the way of creating objects. These design patterns are used
+when a decision must be made at the time of instantiation of a class (i.e., creating an object of a class)
+**/
